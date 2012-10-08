db311659a3f219eed4b68a00e3a799c2cb1460a6
Waylon Calabrese <hiwaylon@gmail.com>
2012-10-08 06:52:22 -0500

    Update tests/test_entity.py
    Added note about event trigging in Entities.

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -11,6 +11,14 @@ from feb.testing.helpers import StubBehavior
 class TestEntity(unittest.TestCase):
     def test_it_should_map_events_to_methods(self):
         stub_behavior = StubBehavior()
+        
+        # NOTE(lwc): The preferred way of triggering a behavior
+        # in an entity is via eventing.
+        #
+        # For example:
+        # events.send("do_it", stub_entity, 1, 2, "three").
+        # or
+        # DoItEvent.send(stub_entity, 1, 2, "three")
 
         # Maps "do_it" to stub_behavior, i.e. blah.do_it() -->
         # stub_behavior.execute().
